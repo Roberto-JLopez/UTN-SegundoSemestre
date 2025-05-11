c852db6a4036d12eb2836de6db33600ada1969e4
Roberto Jose Lopez <Roberlopez1105@gmail.com>
2025-05-11 20:07:33 -0300

    reaordenando archivos

---
+++ Portafolio/Portafolio-Vuejs-RobertoLopez/src/components/InteresesComponente.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+//Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Desarrollo de Software y Programación: Comencé a estudiar programación en 2024 en la UTN San Rafael. Me apasiona la programación y me esfuerzo por aprender y mejorar mis habilidades en este campo.',
+    'Deportes y Actividades al Aire Libre: Disfruto de los deportes como el pádel y el fútbol, que practico regularmente para mantenerme activo. También me gustan las actividades al aire libre, como acampar y pescar, que me permiten desconectar y disfrutar de la naturaleza',
+    'Tecnología e Innovación: Me interesa explorar nuevas tendencias tecnológicas que amplíen mis conocimientos, especialmente en áreas relacionadas con la programación y el desarrollo de aplicaciones',
+    'Viajes y Tiempo con Familia y Amigos: Me encanta viajar y explorar nuevos lugares. También valoro mucho el tiempo que paso con mi familia y amigos, disfrutando de momentos juntos.'
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+    <!-- <div class="intereses-contenedor">
+        <ul class="contenedor-lista">
+            <li class="item">
+                Desarrollo de Software de Código Abierto: Contribuyo a proyectos en GitHub, colaborando con otros
+                desarrolladores para mejorar herramientas y librerías populares.
+            </li>
+            <li class="item">
+                Deportes al Aire Libre: Disfruto del senderismo y el ciclismo de montaña, actividades que practico
+                regularmente para mantener un equilibrio entre el trabajo y la vida personal.
+            </li>
+            <li class="item">
+                Tecnología de Innovación: Me apasiona explorar nuevas tendencias tecnológicas como la inteligencia
+                artificial y el desarrollo de aplicaciones móviles.
+            </li>
+            <li class="item">
+                Fotografía: Practico la fotografía como un hobby, lo que me ayuda a mantener una perspectiva creativa
+                tanto en mi vida profesional como personal.
+            </li>
+        </ul>
+    </div> -->
+</template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    padding: 1rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 1rem;
+    margin-bottom: .5rem;
+    color: var(--vt-c-white-soft);
+    font-size: 1.4rem;
+    text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+}
+
+.item {
+    margin-bottom: 1rem;
+}
+</style>
